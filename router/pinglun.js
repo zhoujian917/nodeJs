ef59b46b2bf14c0ec5b0428df98a9f0cfa8253f9
zhoujian917 <1617285209@qq.com>
2017-04-04 17:11:09 +0800

    添加

--- router/pinglun.js
+++ router/pinglun.js
@@ -7,12 +7,19 @@ const express = require('express'),
 router.get("/",(req,res)=>{
     res.render("/");
 });
+
 router.post("/",(req,res)=>{
+    console.log(111);
    console.log(req.body);
     if(req.cookies.login.name==undefined){
         res.locals.res="请登录!"
         return;
     }
+    mysql("insert into pinglun values (0,?,?,?)",[],(err,data)=>{
+        if(err){
+           return;
+        }
+    });
 });
 
 module.exports = router;
